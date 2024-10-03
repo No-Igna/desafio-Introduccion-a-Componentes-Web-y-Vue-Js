5ada4b130416e1222d577b3963aa2f640d52f246
josé Pérez <j.igna.perez@gmail.com>
2024-10-03 19:15:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import carnet from './components/Carnet.vue'
+</script>
+
+<template>
+   <carnet/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
